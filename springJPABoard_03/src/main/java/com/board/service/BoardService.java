0bdc3e6ff5113d492a1990e1f98c8a2410e24d6d
y9828 <y9828@naver.com>
2026-01-29 14:21:37 +0900

    [생성] register생성

---
+++ springJPABoard_03/src/main/java/com/board/service/BoardService.java
@@ -0,0 +1,11 @@
+package com.board.service;
+
+import com.board.domain.Board;
+
+import com.board.domain.Board;
+
+public interface BoardService {
+	public int register(Board board) throws Exception;
+
+
+}
---
+++ springJPABoard_03/src/main/java/com/board/service/BoardServiceImpl.java
@@ -0,0 +1,25 @@
+package com.board.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.board.domain.Board;
+import com.board.repository.BoardRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired
+	private BoardRepository boardRepository;
+
+	@Override
+	public int register(Board b) throws Exception {
+		Board board = boardRepository.save(b);
+        return (board != null) ? 1 : 0;
+	}
+
+}
