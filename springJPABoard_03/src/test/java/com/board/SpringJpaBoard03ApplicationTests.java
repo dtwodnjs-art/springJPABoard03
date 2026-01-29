ebbcdfac4c2f585de9424ac5c709c31d73b9d048
y9828 <y9828@naver.com>
2026-01-29 13:46:05 +0900

    [create] springJPABoard 생성

---
+++ springJPABoard_03/src/test/java/com/board/SpringJpaBoard03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBoard03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
